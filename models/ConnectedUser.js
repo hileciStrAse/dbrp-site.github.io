6f9e7c9f2befca4fdeb2e84907fb84640aef921c
hileciStrAse <80063532+hileciStrAse@users.noreply.github.com>
2025-05-18 20:56:42 +0400

    Update ConnectedUser.js

--- models/ConnectedUser.js
+++ models/ConnectedUser.js
@@ -1,21 +1,22 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../server').sequelize; // sequelize instanceını server.js-dən import edin
 
-const ConnectedUser = sequelize.define('ConnectedUser', {
-    discordId: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    connectedAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
-    }
-});
+module.exports = (sequelize, DataTypes) => { // sequelize instance-nı arqument kimi qəbul edin
+    const ConnectedUser = sequelize.define('ConnectedUser', {
+        discordId: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        connectedAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    });
 
-module.exports = ConnectedUser; 
+    return ConnectedUser; // Modeli export edin
+}; 
