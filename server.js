e5d4c6d79be38ae6149ee07022f28fbbb6f93ac7
hileciStrAse <80063532+hileciStrAse@users.noreply.github.com>
2025-05-23 13:13:44 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -80,8 +80,8 @@ passport.use(new DiscordStrategy({
     clientSecret: process.env.DISCORD_CLIENT_SECRET,
     callbackURL: process.env.NODE_ENV === 'production' 
         ? process.env.DISCORD_REDIRECT_URI 
-        : process.env.LOCAL_REDIRECT_URI || 'http://localhost:3000/auth/discord/callback',
-    scope: ['identify', 'guilds', 'guilds.channels.read', 'guilds.members.read', 'email']
+        : process.env.LOCAL_REDIRECT_URI || 'http://dbrpbot.onrender.com/auth/discord/callback',
+    scope: ['identify', 'guilds', 'guilds.members.read', 'email']
 }, async function(accessToken, refreshToken, profile, done) {
     try {
         let user = await User.findOneAndUpdate(
